532ac53abf6b49c70d378046ace265d8b62c6b9d
Den Skvortsov <48939274+skvortsovden@users.noreply.github.com>
2021-12-16 20:36:41 +0200

    Handle multiple API endpoints

--- logs_forwarder.py
+++ logs_forwarder.py
@@ -7,35 +7,47 @@ import os
 import logging
 
 app = Flask(__name__)
-logs_api_endpoint = os.environ.get('LOGS_API_ENDPOINT')
+logs_api_endpoints = os.environ.get('LOGS_API_ENDPOINTS')
 
 # New Relic Logs API
 class HeaderlessLogAPI:
 
-    def __init__(self, endpoint) -> None:
-        self.endpoint = endpoint
+    def __init__(self) -> None:
+        self.endpoint = None
     
     # Send message to Logs API endpoint
     def send_message(self, data):
-        try:
+
             print(f"headerlessLogAPI send_message")
             # Decode data
             decoded_data = data.decode('utf8').replace("'", '"')
 
-            # Retrieve JSON object from incoming data
-            json_data = re.search('{(.*)}', decoded_data)
-            json_string = "{" + json_data.group(1)+ "}"
-            json_object = json.loads(json_string)
-            app.logger.info(f"Outcoming data: {json_object}")
-            # Send retrieved JSON with POST request to Logs API endpoint
-            repsonse = requests.post(url=self.endpoint, json=json_object)
+            for line in decoded_data.split('\\n'):
+                # Retrieve JSON object from incoming data
+                json_data = re.search('{(.*)}', line)
+                json_string = "{" + json_data.group(1)+ "}"
+                json_object = json.loads(json_string)
+                app.logger.info(f"Outcoming data: {json_object}")
 
-            # Print response
-            app.logger.info(f"New Relic Log API response status code: {repsonse.status_code}")
-            app.logger.info(f"New Relic Log API response text: {repsonse.text}")
-        except HTTPError as e:
-            # Print error
-            print(e)
+                # Get API endpoint for incoming data entity
+                if logs_api_endpoints:
+                    logs_api_endpoints_json = json.loads(logs_api_endpoints)
+                    for key, value in logs_api_endpoints_json.items():
+                        if key == json_object['entity.name']:
+                            logs_api_endpoint = value
+                    # Send retrieved JSON with POST request to Logs API endpoint
+                    app.logger.info(f"Sending data to New Relic for entity: {json_object['entity.name']}")
+                    try:
+                        repsonse = requests.post(url=logs_api_endpoint, json=json_object)
+                        # Print response
+                        app.logger.info(f"New Relic Log API response status code: {repsonse.status_code}")
+                        app.logger.info(f"New Relic Log API response text: {repsonse.text}")
+                    except HTTPError as e:
+                        # Print error
+                        print(e)
+                else:
+                    app.logger.info(f"Endpoint NOT_FOUND for: {json_object['entity.name']}")
+
 
 @app.route('/')
 def index():
@@ -53,7 +65,7 @@ if __name__ != '__main__':
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
 
-    logs_api = HeaderlessLogAPI(endpoint=logs_api_endpoint)
+    logs_api = HeaderlessLogAPI()
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', debug=True)
